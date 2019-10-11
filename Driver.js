a5bd7c9edd51e93ed381616b3885dcba29cc72fc
cdb76 <43348262+cdb76@users.noreply.github.com>
2019-10-10 21:43:08 -0500

    update to driver

--- Driver.js
+++ Driver.js
@@ -1,6 +1,6 @@
-const port = 8080,
-			express = require("express"),
-			app = express();
+const express = require('express')
+const app = express()
+const port = 80
 app.get("/version", (req, res) => {
  res.send(`This is version 0 of the HotBurger service`);
 });
