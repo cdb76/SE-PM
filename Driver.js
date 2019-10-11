5c1951d582ee0d611b1370b88d63f797db2cfa35
cdb76 <43348262+cdb76@users.noreply.github.com>
2019-10-11 16:05:14 -0500

    Add files via upload

--- Driver.js
+++ Driver.js
@@ -7,24 +7,24 @@ var util = require('util');
 var logger = fs.createWriteStream('log.log', { flags: 'w' });
 
 app.get('/', (req, res) => {
- logger.write(`Connected to Route: / \n`);
+ logger.write("Connected to Route: / \n");
  res.send('Go to /version or /logs');
- logger.write(`Responded to Route: / \n`);
+ logger.write("Responded to Route: / \n");
 });
 
 app.get("/version", (req, res) => {
- logger.write(`Connected to Route: /version \n`);
+ logger.write("Connected to Route: /version \n");
  res.send(`This is version 0 of the HotBurger service`);
- logger.write(`Responded to Route: /version \n`);
+ logger.write("Responded to Route: /version \n");
 });
 
 app.get("/logs", (req, res) => {
- logger.write(`Connected to Route: /logs \n`);
+ logger.write("Connected to Route: /logs \n");
  var logContents = fs.readFileSync('log.log').toString();
  res.send(logContents);
- logger.write(`Responded to Route: /version \n`);
+ logger.write("Responded to Route: /version \n");
 });
 
 app.listen(port, () => {
- logger.write(`Server running on port: ${port} \n`);
+ logger.write("Server running on port: ${port} \n");
 });
