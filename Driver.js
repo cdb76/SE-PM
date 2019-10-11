448335f756b2ee8b0871b42bf373f35ba37cff23
cdb76 <43348262+cdb76@users.noreply.github.com>
2019-10-10 21:47:16 -0500

    Added home /

--- Driver.js
+++ Driver.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 80
+
+app.get('/', (req, res) => {
+ res.send(str.link("https://google.com"))
+});
+
 app.get("/version", (req, res) => {
  res.send(`This is version 0 of the HotBurger service`);
 });
