2ce39bf573e07bc91ff98156211f75fce7f3bab8
cdb76 <43348262+cdb76@users.noreply.github.com>
2019-10-11 16:44:41 -0500

    <br/> not /n

--- Driver.js
+++ Driver.js
@@ -7,24 +7,24 @@ var util = require('util');
 var logger = fs.createWriteStream('log.log', { flags: 'w' });
 
 app.get('/', (req, res) => {
- logger.write(('Connected to Route0: / ') + '\n');
+ logger.write(('Connected to Route0: / ') + '<br/>');
  res.send('Go to /version or /logs');
- logger.write('Responded to Route: / \r\n');
+ logger.write('Responded to Route: / <br/>');
 });
 
 app.get("/version", (req, res) => {
- logger.write('Connected to Route: /version \n');
+ logger.write('Connected to Route: /version <br/>');
  res.send(`This is version 0 of the HotBurger service`);
- logger.write('Responded to Route: /version \n');
+ logger.write('Responded to Route: /version <br/>');
 });
 
 app.get("/logs", (req, res) => {
- logger.write('Connected to Route: /logs \n');
+ logger.write('Connected to Route: /logs <br/>');
  var logContents = fs.readFileSync('log.log', 'utf8');
  res.send(logContents);
- logger.write('Responded to Route: /version \n');
+ logger.write('Responded to Route: /version <br/>');
 });
 
 app.listen(port, () => {
- logger.write('Server running on port: ' + port);
+ logger.write('Server running on port: ' + port + '<br/>');
 });
