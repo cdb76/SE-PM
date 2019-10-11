0c538ea4adc9dea62a9eea60e9b8c38944cb3024
cdb76 <43348262+cdb76@users.noreply.github.com>
2019-10-11 15:45:17 -0500

    changed logs to string

--- Driver.js
+++ Driver.js
@@ -4,32 +4,31 @@ const port = 80
 
 var fs = require('fs');
 var util = require('util');
-var logFile = fs.createWriteStream('log.log', { flags: 'w' });
+var logger = fs.createWriteStream('log.txt', { flags: 'w' });
 
 console.log = function () {
   logFile.write(util.format.apply(null, arguments) + '\n');
 }
 
 app.get('/', (req, res) => {
- console.log(`Connected to Route: /`);
+ logger.write(`Connected to Route: /`);
  res.send('Go to /version or /logs');
- console.log(`Responded to Route: /`);
+ logger.write(`Responded to Route: /`);
 });
 
 app.get("/version", (req, res) => {
- console.log(`Connected to Route: /version`);
+ clogger.write(`Connected to Route: /version`);
  res.send(`This is version 0 of the HotBurger service`);
- console.log(`Responded to Route: /version`);
+ logger.write(`Responded to Route: /version`);
 });
 
 app.get("/logs", (req, res) => {
- console.log(`Connected to Route: /logs`);
- var fs = require('fs');
- var buffer = new Buffer(fs.readFileSync('log.log','utf8'));
- res.send(Buffer.toString());
- console.log(`Responded to Route: /version`);
+ logger.write(`Connected to Route: /logs`);
+ var logContents = fs.readFileSync('log.log').toString();
+ res.send(logContents);
+ logger.write(`Responded to Route: /version`);
 });
 
 app.listen(port, () => {
- console.log(`Server running on port: ${port}`);
+ logger.write(`Server running on port: ${port}`);
 });
