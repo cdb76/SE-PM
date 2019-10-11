ffd7b00cbd030eb9c40cc44c8f9cb03153b1f610
cdb76 <43348262+cdb76@users.noreply.github.com>
2019-10-11 15:58:03 -0500

    new lines

--- Driver.js
+++ Driver.js
@@ -7,24 +7,24 @@ var util = require('util');
 var logger = fs.createWriteStream('log.log', { flags: 'w' });
 
 app.get('/', (req, res) => {
- logger.write(`Connected to Route: /`);
+ logger.write(`Connected to Route: / \n`);
  res.send('Go to /version or /logs');
- logger.write(`Responded to Route: /`);
+ logger.write(`Responded to Route: / \n`);
 });
 
 app.get("/version", (req, res) => {
- logger.write(`Connected to Route: /version`);
+ logger.write(`Connected to Route: /version \n`);
  res.send(`This is version 0 of the HotBurger service`);
- logger.write(`Responded to Route: /version`);
+ logger.write(`Responded to Route: /version \n`);
 });
 
 app.get("/logs", (req, res) => {
- logger.write(`Connected to Route: /logs`);
+ logger.write(`Connected to Route: /logs \n`);
  var logContents = fs.readFileSync('log.log').toString();
  res.send(logContents);
- logger.write(`Responded to Route: /version`);
+ logger.write(`Responded to Route: /version \n`);
 });
 
 app.listen(port, () => {
- logger.write(`Server running on port: ${port}`);
+ logger.write(`Server running on port: ${port} \n`);
 });
