5b79fda32ed10a0e089fb3f71f7574a6dd410c5a
cdb76 <43348262+cdb76@users.noreply.github.com>
2019-10-11 16:32:45 -0500

    Add files via upload

--- Driver.js
+++ Driver.js
@@ -20,10 +20,8 @@ app.get("/version", (req, res) => {
 
 app.get("/logs", (req, res) => {
  logger.write('Connected to Route: /logs \n');
- fs.readFileSync('log.log', 'utf8', function(err, logContents){
-	 if(err) throw err;
-	 res.send(logContents);
- });
+ var logContents = fs.readFileSync('log.log', 'utf8');
+ res.send(logContents);
  logger.write('Responded to Route: /version \n');
 });
 
