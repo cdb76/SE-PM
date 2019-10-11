7cc6de4860885767c98db9db70f5f106e51b8741
cdb76 <43348262+cdb76@users.noreply.github.com>
2019-10-11 15:34:13 -0500

    update logs

--- Driver.js
+++ Driver.js
@@ -6,30 +6,30 @@ var fs = require('fs');
 var util = require('util');
 var logFile = fs.createWriteStream('log.log', { flags: 'w' });
 
-my.log = function () {
+console.log = function () {
   logFile.write(util.format.apply(null, arguments) + '\n');
 }
 
 app.get('/', (req, res) => {
- my.log(`Connected to Route: /`);
+ console.log(`Connected to Route: /`);
  res.send('Go to /version or /logs');
- my.log(`Responded to Route: /`);
+ console.log(`Responded to Route: /`);
 });
 
 app.get("/version", (req, res) => {
- my.log(`Connected to Route: /version`);
+ console.log(`Connected to Route: /version`);
  res.send(`This is version 0 of the HotBurger service`);
- my.log(`Responded to Route: /version`);
+ console.log(`Responded to Route: /version`);
 });
 
 app.get("/logs", (req, res) => {
- my.log(`Connected to Route: /logs`);
+ console.log(`Connected to Route: /logs`);
  var fs = require('fs');
  var buffer = new Buffer(fs.readFileSync('index.html','utf8'));
  res.send(Buffer.toString());
- my.log(`Responded to Route: /version`);
+ console.log(`Responded to Route: /version`);
 });
 
 app.listen(port, () => {
- my.log(`Server running on port: ${port}`);
+ console.log(`Server running on port: ${port}`);
 });
