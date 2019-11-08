07446f6e68afd2dca252d4e97ee222ca669ba34c
cdb76 <43348262+cdb76@users.noreply.github.com>
2019-11-08 12:03:19 -0800

    Add files via upload

---
+++ Monitor.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const port = 8080
+
+var fs = require('fs');
+var util = require('util');
+var logger = fs.createWriteStream('../logs/log.log', { flags: 'wa' });
+
+app.get("/gettotal", (req, res) => {
+	res.send(`/gettotal`);
+});
+app.get("/gettopseller", (req, res) => {
+	res.send(`/gettopseller`);
+});
+app.get("/getrequestcount", (req, res) => {
+	res.send(`/getrequestcount`);
+});
+app.get("/getrequeststatus", (req, res) => {
+	res.send(`/getrequeststatus`);
+});
+app.get("/getrequesttime", (req, res) => {
+	res.send(`/getrequesttime`);
+});
+
+app.listen(port, () => {
+ logger.write('Server running on port: ' + port + '<br/>');
+});
