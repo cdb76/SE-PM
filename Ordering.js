a44f84c9b868d17b754d9ed17f858eb3741a3680
cdb76 <43348262+cdb76@users.noreply.github.com>
2019-11-09 18:54:22 -0800

    Add files via upload

--- Ordering.js
+++ Ordering.js
@@ -10,7 +10,7 @@ app.use(express.json())
 
 app.get('/', (req, res) => {
  logger.write('GET:/\n');
- res.send('Go to /version or /logs');
+ res.send('Welcome to the ordering service!');
  logger.write('SUCCESS/\n');
 });
 
@@ -18,6 +18,12 @@ app.get("/version", (req, res) => {
  logger.write('GET:/version\n');
  res.send(`This is version 1 of the HotBurger service`);
  logger.write('SUCCESS:/version\n');
+});
+
+app.get("/getmenu", (req, res) => {
+ logger.write('GET:/getmenu\n');
+ res.send(`Hotdog: $20 <br/> Hamburger: $35 <br/> Soda: $4 <br/> Cookie: $6 <br/>`);
+ logger.write('SUCCESS:/getmenu\n');
 });
 
 app.post('/purchase', function (req, res) {
