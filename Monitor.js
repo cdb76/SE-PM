b8b227cb0d98a9ceb9bdc9daa9a30633e6b87a94
cdb76 <43348262+cdb76@users.noreply.github.com>
2019-11-09 17:40:14 -0800

    Add files via upload

--- Monitor.js
+++ Monitor.js
@@ -8,29 +8,29 @@ var util = require('util');
 var logger = fs.createWriteStream('../logs/log.log', { flags: 'a' });
 
 app.get("/gettotal", (req, res) => {
-	logger.write(('GET:/gettotal\n'));
+	logger.write('GET:/gettotal\n');
 	res.send(`/gettotal ` + info.getTotal());
-	logger.write(('SUCCESS:/gettotal\n'));
+	logger.write('SUCCESS:/gettotal\n');
 });
 app.get("/gettopseller", (req, res) => {
-	logger.write(('GET:/gettopseller\n');
+	logger.write('GET:/gettopseller\n');
 	res.send(`/gettopseller`);
-	logger.write(('SUCCESS:/gettopseller\n'));
+	logger.write('SUCCESS:/gettopseller\n');
 });
 app.get("/getrequestcount", (req, res) => {
-	logger.write(('GET:/getrequestcount\n'));
+	logger.write('GET:/getrequestcount\n');
 	res.send(`/getrequestcount`);
-	logger.write(('SUCCESS:/getrequestcount\n'));
+	logger.write('SUCCESS:/getrequestcount\n');
 });
 app.get("/getrequeststatus", (req, res) => {
-	logger.write(('GET:/getrequeststatus\n'));
+	logger.write('GET:/getrequeststatus\n');
 	res.send(`/getrequeststatus`);
-	logger.write(('SUCCESS:/getrequeststatus\n'));
+	logger.write('SUCCESS:/getrequeststatus\n');
 });
 app.get("/getrequesttime", (req, res) => {
-	logger.write(('GET:/getrequesttime\n'));
+	logger.write('GET:/getrequesttime\n');
 	res.send(`/getrequesttime`);
-	logger.write(('SUCCESS:/getrequesttime\n'));
+	logger.write('SUCCESS:/getrequesttime\n');
 });
 
 app.get("/logs", (req, res) => {
