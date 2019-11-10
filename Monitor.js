b3debef9608d52359a966993c3752454f7aee92c
cdb76 <43348262+cdb76@users.noreply.github.com>
2019-11-09 16:18:21 -0800

    Add files via upload

--- Monitor.js
+++ Monitor.js
@@ -32,43 +32,6 @@ app.get("/getrequesttime", (req, res) => {
 	logger.write(('SUCCESS:/getrequesttime') + '<br/>');
 });
 
-function parseLog() {
-	var hotdogSale;
-	var hamburgerSale;
-	var sodaSale;
-	var cookieSale;
-	var requestCount;
-	var requestStatus;
-	var requestTime;
-	
-	var logContents = fs.readFileSync('../logs/log.log', 'utf8').toString().split('<br/>');
-	
-	for(i in logContents) {
-		var info = logContents[i].split(":");
-		if("POST" == info[0]) {
-			if("Hotdog" == info[1]) {
-				hotdogSale = hotdogSale + info[2];
-			}
-			else if("Hamburger" == info[1]) {
-				hamburgerSale = hamburgerSale + info[2];
-			}
-			else if("Soda" == info[1]) {
-				sodaSale = sodaSale + info[2];
-			}
-			else if("Cookie" == info[1]) {
-				cookieSale = cookieSale + info[2];
-			}
-			requestCount++;
-		}
-		else if ("GET" == info[0]) {
-			requestCount++;
-		}
-		else if ("SUCCESS" == info[1]) {
-				requestStatus = logContents[i];
-		}
-	}
-};
-
 app.listen(port, () => {
  logger.write('Server running on port: ' + port + '<br/>');
 });
