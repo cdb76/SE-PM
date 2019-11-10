a810ed414a5a67fbba0b40c665472641f00e60d2
cdb76 <43348262+cdb76@users.noreply.github.com>
2019-11-09 18:20:47 -0800

    Add files via upload

--- getMonitorInfo.js
+++ getMonitorInfo.js
@@ -1,3 +1,5 @@
+var fs = require('fs');
+
 exports.getTotal = function() {
 	var logContents = fs.readFileSync('../logs/log.log', 'utf8').toString().split('\n');
 	var total = 0;
@@ -6,7 +8,7 @@ exports.getTotal = function() {
 		
 		if(info[0] == "POST") {
 			if(info[1] == "Hotdog") {
-				return "TEST";
+				//return "TEST";
 				total = total + (info[2] * 20);
 			}
 		}
