cc22650b3c1334c69bb0b16ee0bb53868bf2272e
cdb76 <43348262+cdb76@users.noreply.github.com>
2019-11-09 18:24:16 -0800

    Add files via upload

--- getMonitorInfo.js
+++ getMonitorInfo.js
@@ -8,8 +8,16 @@ exports.getTotal = function() {
 		
 		if(info[0] == "POST") {
 			if(info[1] == "Hotdog") {
-				//return "TEST";
 				total = total + (info[2] * 20);
+			}
+			else if(info[1] == "Hamburger") {
+				total = total + (info[2] * 35);
+			}
+			else if(info[1] == "Soda") {
+				total = total + (info[2] * 4);
+			}
+			else if(info[1] == "Cookie") {
+				total = total + (info[2] * 6);
 			}
 		}
 	}
