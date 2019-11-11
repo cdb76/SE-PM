2e3b892dd2e8ad0ad4059b22e1ad101f617a3329
cdb76 <43348262+cdb76@users.noreply.github.com>
2019-11-10 16:37:20 -0800

    Add files via upload

--- getMonitorInfo.js
+++ getMonitorInfo.js
@@ -80,11 +80,31 @@ exports.getRequestCount = function() {
 	return requestCount;
 };
 
-
 exports.getLastRequestStatus = function() {
 	var logContents = fs.readFileSync('../logs/log.log', 'utf8').toString().split('\n');
 	
 	var requestStatus = logContents[(logContents.length)-2]
 	var info = requestStatus.split(":");
 	return info[0];
+};
+
+exports.getLastRequestTime = function() {
+	var logContents = fs.readFileSync('../logs/log.log', 'utf8').toString().split('\n');
+	
+	var requestStatus = logContents[(logContents.length)-2]
+	var info = requestStatus.split(":");
+	return info[0];
+};
+
+exports.getTime = function() {
+	var date_ob = new Date();
+	var date = ("0" + date_ob.getDate()).slice(-2);
+	var month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+	var year = date_ob.getFullYear();
+	var hours = date_ob.getHours();
+	var minutes = date_ob.getMinutes();
+	var seconds = date_ob.getSeconds();
+	var time = year + "-" + month + "-" + date + " " + hours + ";" + minutes + ";" + seconds;
+
+	return time;
 };
