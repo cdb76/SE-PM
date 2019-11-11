0959207f565abcc6699919498fd11760d1df1be7
cdb76 <43348262+cdb76@users.noreply.github.com>
2019-11-10 17:01:47 -0800

    Add files via upload

--- Ordering.js
+++ Ordering.js
@@ -5,6 +5,7 @@ const port = 80
 var fs = require('fs');
 var util = require('util');
 var logger = fs.createWriteStream('../logs/log.log', { flags: 'a' });
+var info = require('./getTime');
 
 app.use(express.json())
 
@@ -15,19 +16,19 @@ app.get('/', (req, res) => {
 });
 
 app.get("/version", (req, res) => {
- logger.write('GET:/version\n');
+ logger.write('GET:/version:' + time.getTime() + '\n');
  res.send(`This is version 1 of the HotBurger service`);
  logger.write('SUCCESS:/version\n');
 });
 
 app.get("/getmenu", (req, res) => {
- logger.write('GET:/getmenu\n');
+ logger.write('GET:/getmenu' + time.getTime() + '\n');
  res.send(`Hotdog: $20 <br/> Hamburger: $35 <br/> Soda: $4 <br/> Cookie: $6 <br/>`);
  logger.write('SUCCESS:/getmenu\n');
 });
 
 app.post('/purchase', function (req, res) {
-	logger.write('POST:' + req.query.item + ':' + req.query.quantity + '\n');
+	logger.write('POST:' + req.query.item + ':' + req.query.quantity + ':' + time.getTime() + '\n');
 	res.send(req.query.item);
 	logger.write('SUCCESS:/purchase<item><quantity>\n');
 });
