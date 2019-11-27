773d7b5eaf724e27adca28f849219ef96c34b0f4
cdb76 <43348262+cdb76@users.noreply.github.com>
2019-11-26 18:34:34 -0600

    fakeInventory service

---
+++ fakeInventory.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const port = 80
+
+var fs = require('fs');
+app.use(express.json())
+
+let hotdogCount = 10;
+let hamburgerCount = 15;
+let sodaCount = 25;
+let cookieCount = 20;
+
+app.get("/getcount", (req, res) => {
+	res.send(req.params.item + ':' + req.params.itemCount + '\n');
+});
+
+app.post('/setcount', function (req, res) {
+	res.send(req.query.item + ':' + req.query.quantity + '\n');
+});
+
+app.listen(port, () => {
+});
