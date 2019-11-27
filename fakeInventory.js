ede31cd5d0838b777945cb2c6232bf3f52021f80
cdb76 <43348262+cdb76@users.noreply.github.com>
2019-11-27 16:55:47 -0600

    Add files via upload

--- fakeInventory.js
+++ fakeInventory.js
@@ -5,28 +5,46 @@ const port = 80
 var fs = require('fs');
 app.use(express.json())
 
-let hotdogCount = 10;
-let hamburgerCount = 15;
-let sodaCount = 25;
-let cookieCount = 20;
+let hotdogCount = "10";
+let hamburgerCount = "15";
+let sodaCount = "25";
+let cookieCount = "20";
 
 app.get("/getcount", (req, res) => {
-	var s = req.query.item;
-	res.send('{"quantity": "25"}');
+	//new
+	if(req.query.item == "Hotdog"){
+		res.send('{"quantity": "10"}');
+	}
+	else if(req.query.item == "Hamburger"){
+		res.send('{"quantity": "15"}');
+	}
+	else if(req.query.item == "Soda"){
+		res.send('{"quantity": "25"}');
+	}
+	else if(req.query.item == "Cookie"){
+		res.send('{"quantity": "20"}');
+	}
+	else{
+		res.send('{"quantity": "-1"}');
+	}
+	
+	//old
+	//var s = req.query.item;
+	//res.send('{"quantity": "25"}');
 });
 
 app.post('/setcount', function (req, res) {
-	if(req.query.item === 'Hotdog'){
-		hotdogCount = parseInt(req.query.quantity,10);
+	if(req.query.item == 'Hotdog'){
+		hotdogCount = req.query.quantity;
 	}
-	else if(req.query.item === 'Hamburger'){
-		hamburgerCount = parseInt(req.query.quantity,10);
+	else if(req.query.item == 'Hamburger'){
+		hamburgerCount = req.query.quantity;
 	}
-	else if(req.query.item === 'Soda'){
-		sodaCount = parseInt(req.query.quantity,10);
+	else if(req.query.item == 'Soda'){
+		sodaCount = req.query.quantity;
 	}
-	else if(req.query.item === 'Cookie'){
-		cookieCount = parseInt(req.query.quantity,10);
+	else if(req.query.item == 'Cookie'){
+		cookieCount = req.query.quantity,10;
 	}
 	else{
 		res.send('Invalid');
