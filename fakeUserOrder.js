80f21798bfab18762c09ac4234e078139cbf1aa0
cdb76 <43348262+cdb76@users.noreply.github.com>
2019-11-27 21:00:52 -0600

    Add files via upload

--- fakeUserOrder.js
+++ fakeUserOrder.js
@@ -4,26 +4,8 @@ var purchase = 'http://18.224.200.58';
 var count = 0;
 while(count < 10){
 	axios.post('http://18.224.200.58/purchase?item=Hotdog&quantity=6');
-	
-	sleep(1000);
-	axios.post(purchase+'/purchase', {
-		item: 'Hamburger',
-		quantity: '15'
-	});
-	sleep(1000);
-	axios.post(purchase+'/purchase', {
-		item: 'Soda',
-		quantity: '15'
-	});
-	sleep(1000);
-	axios.post(purchase+'/purchase', {
-		item: 'Cookie',
-		quantity: '15'
-	});
-	sleep(1000);
+	axios.post('http://18.224.200.58/purchase?item=Hamburger&quantity=6');
+	axios.post('http://18.224.200.58/purchase?item=Soda&quantity=6');
+	axios.post('http://18.224.200.58/purchase?item=Cookie&quantity=6');
 	count++;
-}
-
-function sleep(millis) {
-    return new Promise(resolve => setTimeout(resolve, millis));
 }
