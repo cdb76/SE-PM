075de96f3f3f3d8bf5f59b41f79225dc38217a97
cdb76 <43348262+cdb76@users.noreply.github.com>
2019-11-27 19:01:10 -0600

    Add files via upload

--- fakeUserOrder.js
+++ fakeUserOrder.js
@@ -3,22 +3,22 @@ const axios = require('axios')
 var purchase = 'http://18.224.200.58';
 while(true){
 	axios.post(purchase+'/purchase', {
-		item: 'Hotdog'
+		item: 'Hotdog',
 		quantity: '15'
 	});
 	sleep(1000);
 	axios.post(purchase+'/purchase', {
-		item: 'Hamburger'
+		item: 'Hamburger',
 		quantity: '15'
 	});
 	sleep(1000);
 	axios.post(purchase+'/purchase', {
-		item: 'Soda'
+		item: 'Soda',
 		quantity: '15'
 	});
 	sleep(1000);
 	axios.post(purchase+'/purchase', {
-		item: 'Cookie'
+		item: 'Cookie',
 		quantity: '15'
 	});
 	sleep(1000);
